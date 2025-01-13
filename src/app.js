d9a6013696ed074af456e7c653fff229c909f95c
gautham <gautham.gokulakonda@apxor.com>
2025-01-13 23:46:00 +0530

    data sanitization and schema validations

--- src/app.js
+++ src/app.js
@@ -10,13 +10,74 @@ app.post("/signup", async (req, res) => {
   // console.log(req.body); // will be undefined if json middleware is not used
 
   const user = new User(req.body); // creating a new instance of the User model
-
+  // if we send any data which is not part of schema it will be ignored
   try {
     await user.save();
     res.send("user saved successfully");
   } catch (err) {
     console.log(err);
     res.status(400).send("user not saved");
+  }
+});
+
+// get user by email
+app.get("/users", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length == 0) {
+      res.status(400).send("user not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("user not found");
+  }
+});
+
+// Feed api - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find({});
+    if (user.length == 0) {
+      res.status(400).send("no users found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("user not found");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted");
+  } catch (err) {
+    res.status(400).send("user not found");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const allowedUpdates = ["photoUrl", "about", "gender", "skills", "age"];
+    const isUpdateAllowed = Object.keys(data).every((update) =>
+      allowedUpdates.includes(update)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("update not allowed");
+    }
+    if (data?.skills.length > 5) {
+      throw new Error("skills cannot be more than 5");
+    }
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+    });
+    console.log("user", user);
+    res.send("user updated");
+  } catch (err) {
+    res.status(400).send("user not found");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,55 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      manLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        // this will only run when we create a new user
+
+        // to check the validations while updating the user we need to add "runValidators: true" in the options
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Invalid gender");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+      default: "No description yet",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true } // this adds createdAt and updatedAt fields for each document
+);
 
 const User = mongoose.model("User", userSchema); // always starts with a capital letter
 
